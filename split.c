6b67710dc5f7363089d876d249002c23fef7e083
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-04 14:26:41 +0100

    auto update

--- split.c
+++ split.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:37:46 by ivan              #+#    #+#             */
-/*   Updated: 2024/11/04 14:22:26 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:25:35 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ char	**ft_split(char const *str, char c)
 
 	s = 0;
 	k = 0;
-	res = malloc(sizeof(char *) * count_words(c, (char *)str) + 1);
+	res = (char **)malloc(sizeof(char *) * count_words(c, (char *)str) + 1);
 	if (res == NULL)
 		return (NULL);
 	while (k < count_words(c, (char *)str) && str[s])
