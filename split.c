bc27beddfa9762c95c74af1e5643e012b675a8a3
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-04 14:35:56 +0100

    auto update

--- split.c
+++ split.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:37:46 by ivan              #+#    #+#             */
-/*   Updated: 2024/11/04 14:25:35 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:34:46 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ int	count_words(char c, char *str)
 		else
 			i++;
 	}
+	if (str[i - 1] == c)
+		count--;
 	return (count);
 }
 
