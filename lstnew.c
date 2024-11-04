eef6e5e82728218ac93d8092ecf2d9142be0efd9
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-04 23:29:08 +0100

    auto update

--- lstnew.c
+++ lstnew.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:19:30 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/04 23:25:30 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/04 23:29:00 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	new;
+	t_list	*new;
 	
-	new = malloc(sizeof(t_list) * 1);
-	
+	new = malloc(sizeof(t_list));
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
