3b5cbe5e1d3c0b6dc683849896e94050bca5f3da
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 00:00:08 +0100

    auto update

--- lstsize.c
+++ lstsize.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 23:49:29 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/04 23:58:41 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/05 00:00:01 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	ft_lstsize(t_list *lst)
 {
 	int	i;
 
-	i = 0;
+	if (lst == NULL)
+		return (0);
+	i = 1;
 	while (lst->next != NULL)
 	{
 		lst->next = lst->next->next;
