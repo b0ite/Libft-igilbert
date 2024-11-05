78538dff0107a2683c6bdb8ad49b34fa923cc858
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 16:34:49 +0100

    auto update

--- lstmap.c
+++ lstmap.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lstiter.c                                          :+:      :+:    :+:   */
+/*   lstmap.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 16:12:31 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/05 16:24:06 by igilbert         ###   ########.fr       */
+/*   Created: 2024/11/05 16:24:22 by igilbert          #+#    #+#             */
+/*   Updated: 2024/11/05 16:34:18 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (!lst || !f)
-		return ;
-	if (lst->next)
-		ft_lstiter(lst->next, f);
-	f(lst->content);
+	t_list	*temp;
+	t_list	*ret;
+
+	temp = ft_lstnew(f(lst->content));
+	ret = temp;
+	while (lst->next)
+	{
+		if(!temp)
+			return (NULL);
+		temp->next = ft_lstnew(f(lst->next->content));
+		lst = lst->next;
+		temp = temp->next;
+	}
+	return (ret);
 }
