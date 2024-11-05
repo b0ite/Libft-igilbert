ba127e3bc74b3b3281422ff1623f3d4a7a0f07b4
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 01:42:50 +0100

    auto update

--- lstadd_back.c
+++ lstadd_back.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lstlast.c                                          :+:      :+:    :+:   */
+/*   lstadd_back.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 00:02:18 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/05 00:07:18 by igilbert         ###   ########.fr       */
+/*   Created: 2024/11/05 00:07:39 by igilbert          #+#    #+#             */
+/*   Updated: 2024/11/05 01:42:02 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*ret;
+	t_list	*start;
 
-	if (!lst)
+	start = *lst;
+	if (!lst || !new)
 		return (NULL);
-	ret = malloc(sizeof(t_list));
-	ret = lst;
-	while (ret->next != NULL)
-		ret = ret->next;
-	return (ret);
+	while (start->next != NULL)
+		start = start->next;
+	start->next = new;
 }
