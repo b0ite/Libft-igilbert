b61b95eac71675504c068de0b1b31733c65bc015
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 01:44:44 +0100

    auto update

--- lstadd_back.c
+++ lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 00:07:39 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/05 01:43:02 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/05 01:44:35 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*start;
 
 	start = *lst;
-	if (!lst || !new)
+	if (!lst || !new || !start)
 		return ;
 	while (start->next != NULL)
+	{
+		if (start == NULL)
+			return ;
 		start = start->next;
+	}
 	start->next = new;
 }
