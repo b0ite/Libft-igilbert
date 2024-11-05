dd083e0d6f9f6e9c94c69aa062ecd390c1577915
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 16:36:17 +0100

    auto update

--- lstmap.c
+++ lstmap.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:24:22 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/05 16:34:18 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:36:09 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*temp;
 	t_list	*ret;
 
+	if(!lst || !f || !del)
+		return (NULL);
 	temp = ft_lstnew(f(lst->content));
 	ret = temp;
 	while (lst->next)
