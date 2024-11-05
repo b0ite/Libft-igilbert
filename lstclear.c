5d5f1340dc9cd9dca72281efebe4cdf2c27168ff
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-05 16:10:32 +0100

    auto update

--- lstclear.c
+++ lstclear.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:50:51 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/05 16:10:01 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:10:29 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	if (!(*lst) || !lst || !del)
 		return ;
 	if ((*lst)->next)
-		ft_lstclear((*lst)->next, del);
+		ft_lstclear(&(*lst)->next, del);
 	del((*lst)->content);
 	free(*lst);
 }
