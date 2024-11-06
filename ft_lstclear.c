db044a0da171f111fc5d8395b8b0c04cce14ab4a
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-06 14:39:45 +0100

    auto update

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,35 +6,20 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:50:51 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/06 14:37:53 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:39:36 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	if (!(*lst) || !lst || !del)
 		return ;
 	if ((*lst)->next)
 		ft_lstclear(&(*lst)->next, del);
-	del((*lst)->content);
+	if ((*lst)->content)
+		del((*lst)->content);
 	free(*lst);
 	*lst = NULL;
-}*/
-
-void	ft_lstclear(t_list **lst, void (*del)(void*))
-{
-	t_list *aux;
-
-	if (!*lst)
-		return ;
-	while (*lst)
-	{
-		aux = (*lst)->next;
-		ft_lstdelone(*lst, del);
-		*lst = aux;
-	}
-	*lst = 0;
 }
