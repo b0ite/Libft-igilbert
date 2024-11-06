bbf47c5b10676e1e304a14517ca1bfb427cf20fb
b0ite <99466964+b0ite@users.noreply.github.com>
2024-11-06 14:44:13 +0100

    auto update

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:40:55 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/06 14:42:55 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:44:09 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (!s || !fd)
+	if (!s)
 		return ;
 	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:36:10 by igilbert          #+#    #+#             */
-/*   Updated: 2024/11/06 14:43:26 by igilbert         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:44:03 by igilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putstr_fd(char *str, int fd)
 	int	i;
 
 	i = 0;
-	if (!str || !fd)
+	if (!str)
 		return ;
 	while (str[i])
 	{
